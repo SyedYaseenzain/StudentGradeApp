686079c45ee1b281687f5d2de4b7999e1902f555
SyedYaseenzain <yasee438@gmail.com>
2025-08-20 16:50:52 +0530

    [edit] program.cs

--- StudentGradeApp/Program.cs
+++ StudentGradeApp/Program.cs
@@ -1,33 +1,23 @@
-using Microsoft.EntityFrameworkCore;
-using StudentGradeApp.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<SchoolContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDb")));
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
+app.MapControllers();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Students}/{action=Index}/{id?}");
-
+// IMPORTANT: Listen on the PORT environment variable
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Run($"http://0.0.0.0:{port}");
